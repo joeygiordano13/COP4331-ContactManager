ba12126db0c14910c1ecaf585058949f5bb3ed2b
mangothief <59812186+mangothief@users.noreply.github.com>
2020-10-03 02:10:32 -0400

    FIXED DELETE

--- js/contacts.js
+++ js/contacts.js
@@ -220,28 +220,23 @@ function readCookie()
 function buildTable(data)
 {
     var table = document.getElementById('cookieTable')
-    var length = data.length;
-    
-    if(length === undefined)
-        length = 0;
-        
-        if(length > 0)
-        {
-            for(var i = 0; i < length; i++)
-            {   
-                var row = `<tr>
-                    <td>${data[i].firstname}</td>
-                    <td>${data[i].lastname}</td>
-                    <td>${data[i].email}</td>
-                    <td>${data[i].phonenumber}</td>
-                    <td>${data[i].favoritecookie}</td>
-                    <td>${data[i].datecreated}</td>
-                    <td><button type="edit";class="btn btnEdit" onclick="openWindow1();updateInfo()">Edit</button></td>
-                    <td><button type="delete";class="btn btnDelete"; onclick="deleter(${data[i].contactid});">Delete</button></td>
-                    </tr>`
-                table.innerHTML += row
-            }
+    if(data > 0)
+    {
+        for(var i = 0; i < data.length; i++)
+        {   
+            var row = `<tr>
+                <td>${data[i].firstname}</td>
+                <td>${data[i].lastname}</td>
+                <td>${data[i].email}</td>
+                <td>${data[i].phonenumber}</td>
+                <td>${data[i].favoritecookie}</td>
+                <td>${data[i].datecreated}</td>
+                <td><button type="edit";class="btn btnEdit" onclick="openWindow1();updateInfo()">Edit</button></td>
+                <td><button type="delete";class="btn btnDelete"; onclick="deleter(${data[i].contactid});">Delete</button></td>
+                </tr>`
+            table.innerHTML += row
         }
+    }
 }
 function deleter(buttonID)
 {
