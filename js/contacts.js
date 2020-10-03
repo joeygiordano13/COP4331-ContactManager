586c38c85372fc8bd4d38edce8a79b8c0b3567c8
mangothief <59812186+mangothief@users.noreply.github.com>
2020-10-03 17:11:37 -0400

    remove build table from update button

--- js/contacts.js
+++ js/contacts.js
@@ -251,8 +251,10 @@ function buildTable(jsonData)
     {
         length = data.length;
     }
+
     for(var i = 0; i < length; i++)
     {   
+        console.log(`${data[i].firstname}`);
         var row = `<tr>
             <td>${data[i].firstname}</td>
             <td>${data[i].lastname}</td>
